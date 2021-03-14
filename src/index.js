30173aebd00b99ead7ddffd696ecdd51168bea6d
alikahwaji <ali.kahwaji@nmit.ac.nz>
2021-03-15 11:59:07 +1300

    old work

---
+++ src/index.js
@@ -0,0 +1,16 @@
+console.log('Hello')
+console.log('World')
+
+let a = 3
+let b = 2
+let sum = a + b
+
+function add(a, b) {
+  return a + b
+}
+
+console.log('Aaron where is you lab?')
+
+let x = 10;
+// let sum = add(3, 2)
+// console.log(sum)
