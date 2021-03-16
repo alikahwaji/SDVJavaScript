84ccfd5b82b67be646124ff005c0957281312915
alikahwaji <ali.kahwaji@hotmail.com>
2021-03-17 10:10:57 +1300

    Old commits

--- src/weekthree.js
+++ src/weekthree.js
@@ -1,2 +1,19 @@
-console.log('Hello SDV503')
-console.log('Ali Kahwaji')
+// TODO JavaScript Variables
+
+// Declare a variable first name and assign a value string
+var firstName = 'Ali'
+// Print it 
+console.log(firstName)
+
+const lastName = 'Kahwaji'
+// Print it
+console.log(lastName)
+
+let fullName = firstName + ' ' + lastName
+
+fullName = `My full name is ${firstName} ${lastName}`// ES6
+
+let string = `Hello SDV503 class`
+
+console.log(fullName)
+console.log(string)
